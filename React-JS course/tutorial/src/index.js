d5a9e1569e9439d9d8a0229808cc1a35994d3e98
AzmayenSabil <azmayensabil@gmail.com>
2022-06-23 19:50:21 +0600

    dont know for sure

--- React-JS course/tutorial/src/index.js
+++ React-JS course/tutorial/src/index.js
@@ -1,12 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Greeting() {
-  return <h4>Hello world</h4>;
-}
+//CSS
+import './index.css';
 
-// const Greeting = () => {
-//   return React.createElement('h1', {}, 'Hello world');
-// }
+//vars
+const img = 'https://webthemez.com/wp-content/uploads/2019/07/free-book-cover-mockup-1000x750.jpg'
+ 
 
-ReactDOM.render(<Greeting />, document.getElementById("root"));
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+    </section>
+  );
+};
+
+const Book = () => {
+  return (
+  <article className="book">
+    <Image></Image>
+    <Title></Title>
+    <Author></Author>
+  </article>
+  );
+};
+
+const Image = () => {
+  return (
+    <img src={img} width='400px'></img>);
+};
+
+const Title = () => <h1>A demo Title</h1>;
+const Author = () => <h4 style={{color:'#617d98', fontSize:'0.5rem', marginTop:'0.25rem'}}>A demo Author</h4>;
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
