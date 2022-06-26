e97c6727e1c9e92cc5799f9b527bea391bcc7639
AzmayenSabil <azmayensabil@gmail.com>
2022-06-27 01:56:20 +0600

    went through modern JS sumit

---
+++ Javascript-course/JS-course-sumit/playground.js
@@ -0,0 +1,52 @@
+// function number(){
+//     return 10;
+// }
+
+// const number1 = (n) => {
+//     return n;
+// }
+
+// const number2 = (n) => n;
+
+
+// console.log(number2(70));
+
+// const javascript = {
+//     name: 'javascript',
+//     libraries: ['React', 'Angular'],
+//     printlibraries: function(){
+//         this.libraries.forEach((library) => {
+//             console.log(`${this.name} loves ${library}`);
+//         });
+//     }
+// };
+
+// javascript.printlibraries();
+
+// var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// // var newNumber = numbers.find(function(i) {
+// //     return i>5;
+// // }, "second parameter");
+
+// // console.log(newNumber);
+
+// // var result = numbers.slice(1,3);
+// // console.log(result);
+
+// // var result = numbers.splice(1, 2, 10,12,13,19);
+// // console.log(numbers);
+
+// // var result = numbers.concat(numbers2);
+
+// var result = numbers.map((num) => {
+//     return 2*num;
+// });
+
+// console.log(result);
+// var numbers = [1, 2, 3, 4];
+// var newNumbers = numbers;
+// newNumbers.push(5);
+
+// console.log(newNumbers);
+
